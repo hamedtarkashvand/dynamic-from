250b2b99b92d7bf281c2d9688fc6b20655cbc8d9
htorkashvand <facebook.hamedtarkashvand@gmail.com>
2022-07-18 15:33:56 +0430

    add alert for user

--- src/Hooks/useCreateUser.js
+++ src/Hooks/useCreateUser.js
@@ -2,6 +2,7 @@ import React from 'react';
 import * as api from './../Api'
 import { useDispatch } from 'react-redux';
 import { createUser , failedUser } from '../Redux/Actions'
+import {toast} from "react-toastify";
 
 
 const useCreateUser = () =>  {
@@ -10,9 +11,11 @@ const useCreateUser = () =>  {
      const fetch = (formData) => {
          setIsLoading(true);
           api.createUser(formData).then(()=>{
+              toast.success('susses new user');
               dispatch(createUser(formData));
               setIsLoading(false)
           }).catch(()=>{
+              toast.error('error new user');
               dispatch(failedUser(formData));
               setIsLoading(false)
           })
